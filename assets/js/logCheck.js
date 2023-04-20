fc9c43323dd19124464d4703bf4388c26a47545f
enioaiello <eaiello@edenschool.fr>
2023-04-20 15:11:08 +0200

    Mise à jour du site

--- assets/js/logCheck.js
+++ assets/js/logCheck.js
@@ -4,11 +4,23 @@ username = localStorage.getItem("username");
 
 const userNameDisplay = () => {
     let token = localStorage.getItem("token");
-    if(token === null){
+    if (token === null) {
         window.location.assign("../index.html");
     } else {
         welcomeText.innerHTML = "Bienvenue " + username + " sur votre panel !";
     }
+    const googleMode = localStorage.getItem("google-mode");
 }  
+
+function googleModeCheck() {
+    if (googleMode === true) {
+        window.location.assign("./google-fake-admin-panel/index.html");
+        console.log("Google-Mode est activé");
+    } else {
+        console.log("Google-Mode est désactivé");
+    }
+}
+
+googleModeCheck();
 
 userNameDisplay();
--- assets/js/logout.js
+++ assets/js/logout.js
@@ -1,5 +1,6 @@
 function logout() {
     alert("Merci d'avoir participé à cette bêta !");
     localStorage.removeItem("token");
+    localStorage.removeItem("username");
     window.location.assign("../index.html");
 }
