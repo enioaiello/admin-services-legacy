6330ba1b172f3c8185647dcc3cd558eab9fdebcd
Enio <eaiello@edenschool.fr>
2023-06-03 09:45:53 +0200

    Patch 1

--- assets/js/account-information.js
+++ assets/js/account-information.js
@@ -4,13 +4,20 @@ const username = localStorage.getItem("username");
 const token = localStorage.getItem("token");
 const email = localStorage.getItem("email");
 const password = localStorage.getItem("password");
+const group = localStorage.getItem("group");
 const usernameLabel = document.querySelector("#usernameInformation");
 const tokenLabel = document.querySelector("#tokenInformation");
 const emailLabel = document.querySelector("#emailInformation");
 const passwordLabel = document.querySelector("#passwordInformation");
+const groupLabel = document.querySelector("#groupInformation");
+const groupArticle = document.querySelector("#groupArticle");
 
 if (username) {
     usernameLabel.textContent = username;
+}
+
+if (username === "root") {
+    usernameLabel.textContent = username + " (ancien compte)";
 }
 
 if (token) {
@@ -23,4 +30,9 @@ if (email) {
 
 if (password) {
     passwordLabel.textContent = password;
+}
+
+if (group) {
+    groupLabel.textContent = group;
+    groupArticle.classList.remove("none");
 }
