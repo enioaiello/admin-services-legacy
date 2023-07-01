c543756dccc846e5657ca6b14d8b0df1581db942
Enio <eaiello@edenschool.fr>
2023-07-01 09:36:59 +0200

    Passage en 4.2.1

--- assets/js/pdpChange.js
+++ assets/js/pdpChange.js
@@ -2,11 +2,14 @@ let pdpChange = document.querySelector("#pdpChange");
 
 function changePdp() {
     let userPdp = prompt("Entrez le lien de votre image");
-    if (userPdp === null || userPdp === undefined) {
+    if (userPdp === null || userPdp === undefined || userPdp === "") {
         alert("Photo de profil retirée avec succès !");
+        localStorage.setItem("pdp", "https://www.photoprof.fr/images_dp/photographes/profil_vide.jpg");
+        window.location.reload();
     } else {
         localStorage.setItem("pdp", userPdp);
-        console.log("Photo de profil modifiée avec succès !")
+        console.log("Photo de profil modifiée avec succès !");
+        window.location.reload();
     }
 }
 
